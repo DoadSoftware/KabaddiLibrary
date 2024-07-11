23fd2825093aef8ee4e566d09e093ec414986689
Doad Software Department <doad.software@gmail.com>
2024-07-11 12:46:23 +0530

    Update Bugs

--- src/main/java/com/kabaddi/model/Bugs.java
+++ src/main/java/com/kabaddi/model/Bugs.java
@@ -20,6 +20,15 @@ public class Bugs {
 	  
 	  @Column(name = "TEXT2")
 	  private String text2;
+	  
+	  @Column(name = "TEXT3")
+	  private String text3;
+	  
+	  @Column(name = "TEXT4")
+	  private String text4;
+	  
+	  @Column(name = "TEXT5")
+	  private String text5;
 
 	  public Bugs() {
 			super();
@@ -61,4 +70,29 @@ public class Bugs {
 	public void setText2(String text2) {
 		this.text2 = text2;
 	}
+
+	public String getText3() {
+		return text3;
+	}
+
+	public void setText3(String text3) {
+		this.text3 = text3;
+	}
+
+	public String getText4() {
+		return text4;
+	}
+
+	public void setText4(String text4) {
+		this.text4 = text4;
+	}
+
+	public String getText5() {
+		return text5;
+	}
+
+	public void setText5(String text5) {
+		this.text5 = text5;
+	}
+	
 }
